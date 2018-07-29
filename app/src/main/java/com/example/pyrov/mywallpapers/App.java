ad1c81420c30760b2bbf2f4b260d460325ad1cad
pyrov <pyrov.sergey@gmail.com>
2018-07-29 12:14:03 +0300

    added certificates to the App class without which some phones did not download content.

--- app/src/main/java/com/example/pyrov/mywallpapers/App.java
+++ app/src/main/java/com/example/pyrov/mywallpapers/App.java
@@ -5,6 +5,12 @@ import android.content.Context;
 
 import com.example.pyrov.mywallpapers.model.PixabayApi;
 
+import java.util.Collections;
+
+import okhttp3.CipherSuite;
+import okhttp3.ConnectionSpec;
+import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -19,9 +25,23 @@ public class App extends Application {
         super.onCreate();
         context = getApplicationContext();
 
+        ConnectionSpec spec = new
+                ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                .tlsVersions(TlsVersion.TLS_1_2)
+                .cipherSuites(
+                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                        CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256)
+                .build();
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectionSpecs(Collections.singletonList(spec))
+                .build();
+
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
         pixabayApi = retrofit.create(PixabayApi.class);
     }
@@ -33,5 +53,4 @@ public class App extends Application {
     public static PixabayApi getPixabayApi() {
         return pixabayApi;
     }
-
 }
--- app/src/main/java/com/example/pyrov/mywallpapers/model/PixabayApi.java
+++ app/src/main/java/com/example/pyrov/mywallpapers/model/PixabayApi.java
@@ -6,6 +6,6 @@ import retrofit2.http.Query;
 
 public interface PixabayApi {
     // "?key=9665918-5c24fd510db60ddcbef834d2e&image_type=photo&order=popular&pretty=true&orientation=vertical&per_page=200"
-    @GET("?key=9665918-5c24fd510db60ddcbef834d2e&image_type=photo&order=popular&orientation=vertical&pretty=true&safesearch=true&per_page=50")
+    @GET("?key=9665918-5c24fd510db60ddcbef834d2e&image_type=photo&order=popular&orientation=vertical&pretty=true&safesearch=true&per_page=200")
     Call<MyResponse> getData(@Query("q") String q);
 }
