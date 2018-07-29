a4b55a7dda07203469a1b734c7ff1afaf2fb8819
pyrov <pyrov.sergey@gmail.com>
2018-07-30 00:34:28 +0300

    fixed minor bug

--- app/src/main/java/com/example/pyrov/mywallpapers/SettingsActivity.java
+++ app/src/main/java/com/example/pyrov/mywallpapers/SettingsActivity.java
@@ -52,6 +52,5 @@ public class SettingsActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-
     }
 }
--- app/src/main/java/com/example/pyrov/mywallpapers/model/dto/MyResponse.java
+++ app/src/main/java/com/example/pyrov/mywallpapers/model/dto/MyResponse.java
@@ -1,7 +1,5 @@
 package com.example.pyrov.mywallpapers.model.dto;
 
-import com.example.pyrov.mywallpapers.model.dto.HitsItem;
-
 import java.util.List;
 
 public class MyResponse {
