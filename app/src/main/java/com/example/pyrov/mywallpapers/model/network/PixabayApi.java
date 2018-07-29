19b73e4c301c83775765e4e2f59bbc8585c3ee80
pyrov <pyrov.sergey@gmail.com>
2018-07-30 00:39:50 +0300

    fixed minor bug

--- app/src/main/java/com/example/pyrov/mywallpapers/model/network/PixabayApi.java
+++ app/src/main/java/com/example/pyrov/mywallpapers/model/network/PixabayApi.java
@@ -7,6 +7,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface PixabayApi {
-    @GET("?key=9665918-5c24fd510db60ddcbef834d2e&image_type=photo&pretty=true")
+    @GET("?key={MY_KEY}&image_type=photo&pretty=true")
     Call<MyResponse> getData(@Query("q") String q, @Query("order") String order, @Query("orientation") String orientation, @Query("safesearch") String safeSearch, @Query("per_page") String perPage);
 }
